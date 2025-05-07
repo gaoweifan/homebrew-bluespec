d0336d44133cc33e5d4cf4a96db69ebc4dd68eb9
Weifan Gao <gaoweifan@fmsh.com.cn>
2025-05-08 01:00:50 +0800

    initial commit

---
+++ Formula/bsc.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Bsc < Formula
+  desc "Bluespec Compiler (BSC)"
+  homepage "https://github.com/B-Lang-org/bsc"
+  url "https://github.com/B-Lang-org/bsc.git", tag: "2025.01.1"
+  license "BSD-3-Clause"
+  url "https://github.com/B-Lang-org/bsc.git", branch: "main"
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bsc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
