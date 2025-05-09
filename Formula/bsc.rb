20ca0a1524843274d341f2301b2c61a6268b8bef
Weifan Gao <gaoweifan@fmsh.com.cn>
2025-05-09 11:18:36 +0800

    add test for bsc

--- Formula/bsc.rb
+++ Formula/bsc.rb
@@ -37,15 +37,60 @@ class Bsc < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test bsc`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    (testpath/"FibOne.bsv").write <<~BSV
+      (* synthesize *)
+      module mkFibOne();
+        // register containing the current Fibonacci value
+        Reg#(int) this_fib();              // interface instantiation
+        mkReg#(0) this_fib_inst(this_fib); // module instantiation
+        // register containing the next Fibonacci value
+        Reg#(int) next_fib();
+        mkReg#(1) next_fib_inst(next_fib);
+
+        rule fib;  // predicate condition always true, so omitted
+            this_fib <= next_fib;
+            next_fib <= this_fib + next_fib;  // note that this uses stale this_fib
+            $display("%0d", this_fib);
+            if ( this_fib > 50 ) $finish(0) ;
+        endrule: fib
+      endmodule: mkFibOne
+    BSV
+
+    expected_output = <<~EOS
+      0
+      1
+      1
+      2
+      3
+      5
+      8
+      13
+      21
+      34
+      55
+    EOS
+
+    # Checking Verilog generation
+    system "#{bin}/bsc", "-verilog",
+                         "FibOne.bsv"
+
+    # Checking Verilog simulation
+    system "#{bin}/bsc", "-vsim", "verilator",
+                         "-e", "mkFibOne",
+                         "-o", "mkFibOne.vexe",
+                         "mkFibOne.v"
+    assert_match expected_output, shell_output("./mkFibOne.vexe")
+    
+
+    # Checking Bluesim object generation
+    system "#{bin}/bsc", "-sim",
+                         "FibOne.bsv"
+
+    # Checking Bluesim simulation
+    system "#{bin}/bsc", "-sim",
+                         "-e", "mkFibOne",
+                         "-o", "mkFibOne.bexe",
+                         "mkFibOne.ba"
+    assert_equal expected_output, shell_output("./mkFibOne.bexe")
   end
 end
